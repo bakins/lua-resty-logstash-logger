5a08c19152415977fb4f2ebe49374db6054f4343
bakins <brian.akins@turner.com>
2014-04-23 13:27:17 -0400

    allow pushing to redis or to logstash

--- lib/resty/logstash/logger.lua
+++ lib/resty/logstash/logger.lua
@@ -8,6 +8,7 @@ local cjson_encode = cjson_encode
 
 local mp = require "MessagePack"
 local mp_encode = mp.pack
+local redis = require "resty.redis"
 
 local _M = {}
 
@@ -20,6 +21,49 @@ local encoders = {
     msgpack = mp_encode
 }
 
+local function redis_pusher(self, server, data)
+    local red = redis:new()
+    red:set_timeout(self.timeout)
+    local ok, err = red:connect(server.host, server.port)
+    if not ok then
+        return nil, err
+    end
+    red:init_pipeline()
+    local key = self.key
+    for _,v in ipairs(data) do
+        red:lpush(key, v)
+    end
+    local results, err = red:commit_pipeline()
+    if not results then
+        return nil, err
+    end
+    red:set_keepalive()
+    return true
+end
+
+local function logstash_pusher(self, server, data)
+    -- hack to get a trailing newline
+    insert(data, "")
+    data = concat(data, "\n")
+    local sock = tcp()
+    sock:settimeout(self.timeout)
+    local ok, err = sock:connect(server.host, server.port)
+    if not ok then
+        return nil, err
+    end
+    local bytes, err = sock:send(data)
+    if not bytes then
+        return nil, err
+    end
+    sock:set_keepalive()
+    return true
+end
+
+local pushers = {
+    redis = redis_pusher,
+    logstash = logstash_pusher
+}
+
 --- create a new logger.
 -- @tparam table opts a table of options. valid fields are:
 -- * `codec` string. `json` or `msgpack`. defaults to `json`
@@ -27,6 +71,8 @@ local encoders = {
 -- * `interval` number - how often to attempt to push the log buffer to logstash servers in seconds. defaults to 30
 -- * `retries` number - how many times to retry. If the logger gets an error writing to a server, it will select another and retry. After `retries` times, the data buffer is flushed and an error is logged locally. defaults to the number of servers.
 -- * `timeout` number - timeout in ms for all socket operations. defaults to 1000
+-- * `type` string - either `redis` or `logstash`. defaults to `logstash`
+-- * `key` string - only needed when pushing to redis, this is the key to push to. defaults to `logstash`
 -- @treturn resty.logstash.logger a logger
 function _M.new(opts)
     local codec = opts.codec or "json"
@@ -52,6 +98,11 @@ function _M.new(opts)
         insert(hosts, { host: v.host, port, v.port })
     end
 
+    local pusher = pushers[opts.type or "logstash"]
+    if not pusher then
+        return nil, "unknown type"
+    end
+
     local num_servers = #servers
     local self = {
         servers = hosts,
@@ -62,7 +113,9 @@ function _M.new(opts)
         current_server = 1,
         interval = opts.interval or 30000,
         timer_started = false,
-        buffer = {}
+        buffer = {},
+        pusher = pusher,
+        key = opts.key or "logstash"
     }
 
     return setmetatable(self, _mt)
@@ -79,23 +132,15 @@ local tcp = ngx.socket.tcp
 local flush_buffer
 flush_buffer=function(premature, self)
     local buffer = self.buffer
+    local pusher = self.pusher
     if #buffer > 0 then
-        -- hack to get a trailing newline
-        insert(buffer, "")
-        local data = concat(buffer, "\n")
         local retries = self.retries
 
         repeat
             local server = next_server(self)
-            local sock = tcp()
-            sock:settimeout(self.timeout)
-            local ok, err = sock:connect(server.host, server.port)
+            local ok, err = pusher(self, next_server(self), buffer)
             if ok then
-                local bytes, err = sock:send(data)
-                if bytes then
-                    sock:setkeepalive()
-                    break
-                end
+                break
             end
             retries = retries - 1
         until retries < 0
@@ -104,11 +149,14 @@ flush_buffer=function(premature, self)
         end
     end
 
+    self.buffer = {}
+
     if not premature then
         ngx.timer.at(self.interval, handler)
     end
 end
 
+local date = os.date
 --- log data.  The logger will add the required logstash fields to data. Note: this actually adds to an internal buffer and the data is written at `interval` via a timer. Also, this will modify data.
 -- @tparam resty.logstash.logger self
 -- @tparam table data
@@ -121,7 +169,11 @@ function _M.log(self, data)
         end
     end
 
-    -- TODO: add fields required by logstash
+    -- TODO: add all fields required by logstash
+
+    -- logstash requires ISO 8601. format from http://stackoverflow.com/a/20131960
+    data["@timestamp"] = date("!%Y-%m-%dT%TZ", ngx.time())
+
     local data, err = self.encoder(data)
     if not data then
         return nil, err
